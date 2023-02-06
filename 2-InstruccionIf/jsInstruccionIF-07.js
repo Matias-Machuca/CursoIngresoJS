ebfbc97fd0b4417f2e7cb3102d625235df023987
Matias-Machuca <matyoktubre@gmail.com>
2023-02-06 18:07:46 -0300

    Ej 7 IF resuelto

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,16 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
-	
+function mostrar() {
+	let estadoCivil;
 
+	let edad;
 
-}//FIN DE LA FUNCIÓN
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	edad = document.getElementById("txtIdEdad").value;
+
+	edad = parseInt(edad);
+
+	if (edad < 18 && estadoCivil != "Soltero") {
+
+		alert("Es demasiado pequeño para no ser soltero");
+	}
+} 

441c52b0e47dfcb6be5409e135e08f1fae8f3590
Matias-Machuca <matyoktubre@gmail.com>
2023-02-06 17:34:43 -0300

    ejercicio 6 IF resuelto

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,55 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+function mostrar() {
+	let edad;
+
+	edad = document.getElementById("txtIdEdad").value;
+
+	edad = parseInt(edad);
+
+	//Funcion original
+	/*if (edad < 13) {
+		alert("Es niño");
+	}
+	else {
+		if (edad >= 13 && edad <= 17) {
+			alert("Es adolescente");
+		}
+		else {
+			alert("Es adulto");
+		}
+	}*/
+
+	// Agregando una condicion (adulto)
+	/*if (edad < 13) {
+		alert("Es niño");
+	}
+	else {
+		if (edad >= 13 && edad <= 17) {
+			alert("Es adolescente");
+		}
+		else {
+			if(edad < 65) {
+			alert("Es adulto");
+		}
+			else {
+				alert("Es anciano");
+			}
+		}
+	}*/
 
 
+	//Acomodando la funcion y utilizando else if para evitar el anidado
+	//
+	if (edad < 13) {
+		alert("Es niño");
+	}
+	else if (edad >= 13 && edad <= 17) {
+		alert("Es adolescente");
+	}
+	else if (edad < 65) {
+		alert("Es adulto");
+	}
+	else {
+		alert("Es anciano");
+	}
 
-}//FIN DE LA FUNCIÓN
+}

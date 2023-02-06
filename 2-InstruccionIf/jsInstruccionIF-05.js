08436e7f47abc15a264053eaecfe7e93347dd53a
Matias-Machuca <matyoktubre@gmail.com>
2023-02-06 16:12:36 -0300

    Resolucion ej 5 IF

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,19 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+function mostrar() {
+	let edad;
 
-}//FIN DE LA FUNCIÓN
+	edad = document.getElementById("txtIdEdad").value;
+
+	edad = parseInt(edad);
+
+	if (edad < 13 || edad > 17) {
+
+		alert("No es adolescente");
+
+
+		/*pregunto si no estoy dentro del rango
+		if( !(edad >= 13 && edad <= 17)) {
+			alert("No es adolescente");
+		}*/
+	}
+
+}

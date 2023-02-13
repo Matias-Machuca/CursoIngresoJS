f756f472f7beabcbdbc62ec0b8e84a0180a47823
Matias-Machuca <matyoktubre@gmail.com>
2023-02-12 22:17:00 -0300

    ejercicio 4 switch terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,27 @@
+/*
+Enunciado:
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.
+*/
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mes;
+	mes = document.getElementById("txtIdMes").value;
 
-
-
-}//FIN DE LA FUNCIÓN
+	switch(mes) {
+		case "Febrero":
+			alert("Este mes tiene 28 dias.");
+			break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Este mes tiene 30 dias.");
+			break;
+		default:
+			alert("Este mes tiene 31 dias.");	
+	}
+}

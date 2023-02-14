1649d3b472da5c65935661006f3575f2983ec4db
Matias-Machuca <matyoktubre@gmail.com>
2023-02-14 01:24:29 -0300

    ej 4 a terminar

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,11 @@
 /*
 al presionar el botón 
 pedir un número entre 0 y 9 inclusive.*/
+
+
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	let numeroIngresado;
+	numeroIngresado = prompt("Ingrese un número entre 0 y 10.");
 	
-}//FIN DE LA FUNCIÓN
+}

17dd13b6a760cb247fa40132d189e6f77a6cc7a1
Matias-Machuca <matyoktubre@gmail.com>
2023-02-14 16:22:13 -0300

    actualizacion ej 2 de While

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,11 +3,13 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	let condicion;
+	let condicion; // declaro e1 inicializo la variable
 	condicion = 11;
 
-	while(condicion > 1) {
-		condicion = condicion - 1
+	while(condicion > 1) { // variable de control (condicion)
+
+		condicion = condicion - 1 // modificacion de la variable d control
+
 		alert(condicion);
 	}
 }

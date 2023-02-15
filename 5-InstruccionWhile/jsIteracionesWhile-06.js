66371734a5048ae96f24af221fc9e4a93fc5835d
Matias-Machuca <matyoktubre@gmail.com>
2023-02-15 15:18:15 -0300

    ejercicio 6 de While terminado

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -5,24 +5,33 @@
 
 function mostrar()
 {
+	//Declaro variables
 	let numeroIngresado;
-	let acumulador = 0;
 	let promedio;
+	//Declaro e inicializo variables.
+	let acumulador = 0; //Guardo y sumo los número ingresados por el usuario.
+	//Variable de control.
 	let condicion = 0;
 	
+	//Armo la condición:
 	while (condicion < 5) {
-
+		//Pido al usuario los números por prompt.
 		numeroIngresado = prompt("Ingrese un numero");
+		//Parseo los datos ingresados.
 		numeroIngresado = parseInt(numeroIngresado);
 
+		//En la varible acumulador voy guardando los datos ingresados y hago la suma.
 		acumulador = acumulador + numeroIngresado;
 
+		//Modifico la condición para que se ajuste al enunciado:
 		condicion = condicion + 1;
 	}
 
+	//Calculo el promedio usando los datos que ingresó el usuario y guardé previamente
+	//en la variable acumulador.
 	promedio = acumulador / 5;
 	
-	
+	//Muestro por caja de texto la suma acumulada y el promedio.
 	document.getElementById("txtIdSuma").value = acumulador;
 	document.getElementById("txtIdPromedio").value = promedio;
 }

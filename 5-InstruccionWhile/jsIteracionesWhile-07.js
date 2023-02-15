9a3ab3393e760019f10179ffd584c95fa6459669
Matias-Machuca <matyoktubre@gmail.com>
2023-02-15 17:41:46 -0300

    ejercicio 7 de While terminado

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -2,18 +2,55 @@
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
-function mostrar()
-{
+function mostrar() {
 	let seguir;
+	let numeroIngresado;
+	let promedio;
+	let acumulador = 0;
+	let contador = 0;
+
+	do {
+		numeroIngresado = prompt("Ingrese un numero:");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		acumulador = acumulador + numeroIngresado;
+
+		contador = contador + 1;
+
+		//Modificacion de la variable de control.
+		seguir = prompt("¿Quiere ingresar otro numero?");
+
+	} while (seguir == "si");
+
+	promedio = acumulador / contador;
+
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
 
 
-	while(seguir == 's') {
+	/*
+	let seguir = 's';
+	let numeroIngresado;
+	let promedio;
+	let acumulador = 0;
+	let contador = 0;
 
-		alert("Hola!");
-		seguir = prompt("¿Quiere que lo vuelva a saludar?");
+	while (seguir == 's') {
+
+		numeroIngresado = prompt("Ingrese un numero:");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		acumulador = acumulador + numeroIngresado;
+
+		contador = contador + 1;
+
+		//Modificacion de la variable de control.
+		seguir = prompt("¿Quiere ingresar otro numero?");
 	}
 
-	alert("Entonces ya no lo saludo mas.");
+	promedio = acumulador / contador;
 
-	
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
+	*/
 }
